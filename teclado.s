4137267830c420e318e4c9c9bcaa98825a909c16
Manuel A. Lopez <60410203+manuels12@users.noreply.github.com>
2020-11-20 18:34:05 -0800

    Update teclado.s

--- teclado.s
+++ teclado.s
@@ -1,46 +1,11 @@
-@ greet.s - a little asm greeter.
-@ programa corto que obtiene la entrada del teclado y luego lo imprime de nuevo en la pantalla.
+@ primero.s
+@   from thinkingeek.com
+@   http://thinkingeek.com/2013/01/09/arm-assembler-raspberry-pi-chapter-1/
+@   Defines a main function that returns 2 as an exit code.
+@	
 
-
-.section	.bss
-.comm buffer, 48	     @ reserve 48 byte buffer
-
-.section	.data
-msg:
-	.ascii	"** Greeter **\nPlease enter your name: "
-msgLen = . - msg
-msg2:
-	.ascii	"Hello "
-msg2Len = . - msg2
-
-.section	.text
-.globl	_start
-_start:
-
-mov r0, $1		    @ print program's opening message	
-ldr r1, =msg
-ldr r2, =msgLen
-mov r7, $4
-svc $0
-
-mov r7, $3		    @ read syscall
-mov r0, $1		
-ldr r1, =buffer
-mov r2, $0x30
-svc $0
-
-mov r0, $1		    @ print msg2
-ldr r1, =msg2
-ldr r2, =msg2Len
-mov r7, $4
-svc $0
-
-mov r0, $1		    @ now print the user input
-ldr r1, =buffer
-mov r2, $0x30
-mov r7, $4
-svc $0
-
-mov r7, $1	            @ exit syscall
-svc $0		            @ wake kernel
-.end
+	
+	.global main
+	
+main:   mov r0, #2       @ load immediate value 2 into Register r0 
+	bx lr            @ return 2 to Operating Sytem
